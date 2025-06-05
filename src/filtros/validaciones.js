8f157f1864bf637cdbcb766a8582bb9cf63a0528
saucedo <saucedog926@gmail.com>
2025-06-05 19:33:55 -0300

    refactorizando con ternarios

--- src/filtros/validaciones.js
+++ src/filtros/validaciones.js
@@ -1,15 +1,9 @@
 let dicValidaciones = {
-   "stock": (product,val = {}) => {
-        if (val.min && val.max) {
-            return val.min <= product.stock  && product.stock <= val.max
-        }
-        return product.stock > val 
+   "stock": (p,val = {}) => {
+        return val.min && val.max ? val.min <= p.stock  && p.stock <= val.max : p.stock > val
     },
-   "precio": (product,val = {}) => {
-       if (val.min && val.max) {
-           return val.min <= product.precio  && product.precio <= val.max
-       }
-       return product.precio <= val 
+   "precio": (p,val = {}) => {
+       return val.min && val.max ? val.min <= p.precio  && p.precio <= val.max : p.precio <= val
    },
    "nombre": (product,val) => product.nombre.length < val,
    "categoria": (product,val) => val.includes(product.categorias)
